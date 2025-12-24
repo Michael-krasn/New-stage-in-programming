7d93dc9b2f77fcf4cf5f625e3a1547e2de19b194
Mike <scarlettheadliner@gmail.com>
2025-12-24 18:13:12 +0300

    Add files via upload

--- src/models.py
+++ src/models.py
@@ -1,57 +1,92 @@
 class Product:
-    def __init__(self, name: str, description: str, price: float, quantity: int):
+    def __init__(self, name, *args):
         self.name = name
-        self.description = description
-        self.__price = price  # ✅ приватный атрибут
-        self.quantity = quantity
+        if len(args) == 2:
+            self.description = None
+            self.__price = args[0]
+            self.quantity = args[1]
+        elif len(args) == 3:
+            self.description = args[0]
+            self.__price = args[1]
+            self.quantity = args[2]
+        else:
+            self.description = None
+            self.__price = 0
+            self.quantity = 0
 
     @property
     def price(self):
-        """Геттер цены"""
         return self.__price
 
     @price.setter
-    def price(self, new_price):
-        """Сеттер цены с проверкой"""
-        if new_price <= 0:
+    def price(self, value):
+        if value <= 0:
             print("Цена не должна быть нулевая или отрицательная")
         else:
-            self.__price = new_price
+            self.__price = value
 
     @classmethod
     def new_product(cls, data: dict):
         return cls(
-            name=data["name"],
-            description=data["description"],
-            price=data["price"],
-            quantity=data["quantity"],
+            data["name"],
+            data.get("description"),
+            data["price"],
+            data["quantity"],
         )
+
+    def __str__(self):
+        return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        if not isinstance(other, Product):
+            return NotImplemented
+        return self.price * self.quantity + other.price * other.quantity
+
 
 class Category:
-    _category_count = 0
-    _product_count = 0
-
-    def __init__(self, name: str, description: str, products: list = None):
+    def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.__products = products if products else []
-        Category._category_count += 1
-        Category._product_count += len(self.__products)
+        self.products = products if products is not None else []
 
     def add_product(self, product):
-        self.__products.append(product)
-        Category._product_count += 1
-
-    @property
-    def products(self):
-        return "\n".join(
-            [f"{p.name}, {p.price} руб. Остаток: {p.quantity} шт." for p in self.__products]
-        )
-
-    @property
-    def category_count(self):
-        return Category._category_count
+        self.products.append(product)
 
     @property
     def product_count(self):
-        return len(self.__products)
+        return sum(product.quantity for product in self.products)
+
+    def __str__(self):
+        return f"{self.name}, количество продуктов: {self.product_count} шт."
+
+    @property
+    def products_list(self):
+        # Возвращаем строки для удобства
+        return [str(product) for product in self.products]
+
+    def __iter__(self):
+        self._index = 0
+        return self
+
+    def __next__(self):
+        if self._index >= len(self.products):
+            raise StopIteration
+        product = self.products[self._index]
+        self._index += 1
+        return product
+
+
+class CategoryIterator:
+    def __init__(self, category):
+        self._products = category.products
+        self._index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._index >= len(self._products):
+            raise StopIteration
+        product = self._products[self._index]
+        self._index += 1
+        return product
